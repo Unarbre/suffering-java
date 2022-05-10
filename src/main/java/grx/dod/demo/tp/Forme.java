8bf56f858e96976ee455199262ac49c9a69a0739
Simon Halimi <33205321+Unarbre@users.noreply.github.com>
2022-05-10 12:03:38 +0200

    first commit

---
+++ src/main/java/grx/dod/demo/tp/Forme.java
@@ -0,0 +1,17 @@
+package grx.dod.demo.tp;
+
+public abstract class Forme {
+	
+	String type;
+	public static final String RECTANGLE = "R";
+	public static final String CERCLE = "C";
+	public static final String ESPACE = "E";
+	
+	String color;
+	
+	protected Forme(String type, String color) {
+		this.type = type;
+		this.color = color;
+	}
+
+}
